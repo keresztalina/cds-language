91b97595c85575dfd43d307ac929abd3ee03e31d
keresztalina <kereszt98@gmail.com>
2023-03-08 11:24:32 +0100

    ran script

--- run_ak.sh
+++ run_ak.sh
@@ -9,6 +9,7 @@ source ./spacy_env/bin/activate
 # install requirements
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+python3 -m spacy download en_core_web_md
 
 # run code
 python3 src/script.py
